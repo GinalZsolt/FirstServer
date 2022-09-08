34f766a364fd68544dcfe82dd8d6392c4c2bdc5d
ginalzsolt <ginalzsolt@turr.hu>
2022-09-08 10:55:35 +0200

    change

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
-const express = require('express')
+const express = require('express');
+const path  = require('path');
 const app = express()
+const port=3000;
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.get('/', (req, res)=>{
+  res.status(200).sendFile(path.join(__dirname+'/index.html'))
+})
+app.use('/css',express.static(path.join(__dirname+'/cs')))
+app.use(express.urlencoded({extended:true}))
+app.get('/users',(req,res)=>{
+  res.status(200).sendFile(path.join(__dirname+'/users.html'))
+})
+
+app.get('/contacts',(req,res)=>{
+  res.status(200).sendFile(path.join(__dirname+'/contacts.html'))
+})
+
+app.post('/message',(req,res)=>{
+  console.log(req.body)
+  res.send("Kész")
 })
 
 app.listen(3000)
